0c44b1592f3876562e5d7fc961626175061e9df9
kuromi2004 <ashley.24e@gmail.com>
2026-05-12 12:51:17 -0600

    Agregacion de Excepciones y dos service para categorizarlos

---
+++ InfoDynamics.Aplicacion/servicios/Servicios/EmpresaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InfoDynamics.Aplicacion.Abstracts;
+using InfoDynamics.Aplicacion.CustomException;
+using InfoDynamics.Aplicacion.dtos;
+using InfoDynamics.Aplicacion.servicios.IServicios.IServicioMapping;
+using InfoDynamics.Dominio.Entidades;
+using InfoDynamics.Dominio.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfoDynamics.Aplicacion.servicios.Servicios
+{
+
+}
