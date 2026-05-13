60d23e73dcc52dfc2b9169122801d0931fee3ca4
kuromi2004 <ashley.24e@gmail.com>
2026-05-13 01:46:11 -0600

    Agregacion de servicios

---
+++ InfoDynamics.Aplicacion/servicios/Servicios/PeriodoService.cs
@@ -0,0 +1,12 @@
+﻿using InfoDynamics.Aplicacion.Abstracts;
+using InfoDynamics.Aplicacion.CustomException;
+using InfoDynamics.Aplicacion.dtos;
+using InfoDynamics.Aplicacion.servicios.IServicios.IServicioMapping;
+using InfoDynamics.Dominio.Entidades;
+using InfoDynamics.Dominio.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfoDynamics.Aplicacion.servicios.Servicios
+{
+
+}
---
+++ InfoDynamics.Aplicacion/servicios/Servicios/ProyectoService.cs
@@ -0,0 +1,13 @@
+﻿using InfoDynamics.Aplicacion.Abstracts;
+using InfoDynamics.Aplicacion.CustomException;
+using InfoDynamics.Aplicacion.dtos;
+using InfoDynamics.Aplicacion.servicios.IServicios.IServicioMapping;
+using InfoDynamics.Dominio.Entidades;
+using InfoDynamics.Dominio.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfoDynamics.Aplicacion.servicios.Servicios
+{
+    //Separar por funcionalidades es decir validaciones por clases para campos para categoria 
+
+}
