5110060ad3517750347fb5c38e8fbc9a66e59d38
kuromi2004 <ashley.24e@gmail.com>
2026-05-27 23:19:52 -0600

    rdhrd

---
+++ InfoDynamics.Dominio/Entidades/Auditoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoDynamics.Dominio.Entidades
+{
+    public partial class Auditoria
+    {
+        public int id_auditoria { get; set; }
+
+        public int id_registro { get; set; }
+
+        public int id_periodo { get; set; }
+
+        public string codigo { get; set; }
+
+        public DateTime fecha { get; set; }
+
+        public float horas { get; set; }
+
+        public String accion { get; set; }
+   
+
+    }
+}
