2475ca693193054a69b1d65047de65ee3982c12e
L4lito <kennethherdz@gmail.com>
2026-05-29 03:12:17 -0600

    Se agrego un metodo en el Controller y Servicio de RegistroJornada. OJO, en el servicio de Jornada hay un metodo comentado, ya que se realizo una prueba para que se ingresara a la tabla de bd de Usuarios, que si paso. Pero el comentado conecta a Registros para hacer la accion de poder obtener las horas de los empleados y mas tarde obtener a los 3 empleados con mas horas, sin embargo como no hay registros en "SELECT * FROM Registro" sale vacio el arreglo que guarda a los empleados con mas horas. Tambien se agrego un Dto de nombre MEmpleadoDto para tomar el nombre y la id del usuario para acto seguido tomarlo con los metodos antes explicados.

--- InfoDynamics.API/Controllers/UsuarioController.cs
+++ InfoDynamics.API/Controllers/UsuarioController.cs
@@ -50,7 +50,7 @@ namespace InfoDynamics.API.Controllers
         // METODO CAMBIADO PARA QUE SE PUEDAN CONSULTAR EMPLEADOS ASIGNADOR Y NO AJENOS
         [Authorize]
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<UsuarioResponseDTO>> GetById(int id)
+        public async Task<ActionResult> GetById(int id)
         {
             try
             {
@@ -85,19 +85,26 @@ namespace InfoDynamics.API.Controllers
 
                 bool esManager = User.IsInRole("Manager");
 
+                if (!esManager)
+                {
+                    return Forbid();
+                }
+
                 var servicioConcreto = (UsuarioServicio)_usuarioServicio;
 
-                bool pertenece = await servicioConcreto.ManagerTieneEmpleado(
+                var empleado = await servicioConcreto.ObtenerEmpleadoDeManager(
                     usuarioAutenticado,
                     id);
 
-                return Ok(new
+                if (empleado == null)
                 {
-                    managerAutenticado = usuarioAutenticado,
-                    empleadoSolicitado = id,
-                    esManager = esManager,
-                    pertenece = pertenece
-                });
+                    return NotFound(new
+                    {
+                        message = "El empleado no pertenece al manager o no existe."
+                    });
+                }
+
+                return Ok(empleado);
             }
             catch (Exception ex)
             {
